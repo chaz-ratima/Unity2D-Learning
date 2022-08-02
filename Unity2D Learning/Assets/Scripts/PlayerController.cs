f03e2f991e4ad50aa005efa8a8f74873addfe003
chaz-ratima <chaz.ratima@gmail.com>
2022-08-03 02:01:44 +1200

    Update PlayerController.cs
    Added comments

--- Unity2D Learning/Assets/Scripts/PlayerController.cs
+++ Unity2D Learning/Assets/Scripts/PlayerController.cs
@@ -32,15 +32,18 @@ public class PlayerController : MonoBehaviour
         moveHorizontal = Input.GetAxis("Horizontal");
         moveVertical = Input.GetKeyDown(KeyCode.Space);
         
+        // Vertical movement
         if (jumpCount != 0 && moveVertical == true)
         {
                 rb2D.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            // lowers jumpCount by 1 for each "space" jump
                 jumpCount--;
         }
     }
 
     void FixedUpdate()
     {
+        // Horizontal movement
         if (moveHorizontal > 0.1f || moveHorizontal < 0.1f)
         {
             rb2D.AddForce(new Vector2(moveHorizontal * moveSpeed, 0f), ForceMode2D.Impulse);
@@ -53,12 +56,14 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.tag == "Platform")
         {
             isJumping = false;
+            // Resets jumpCount
             jumpCount = 2;
         }
     }
 
     void OnTriggerExit2D(Collider2D collision)
     {
+        // Ground Exit
         if (collision.gameObject.tag == "Platform")
         {
                 isJumping = true;
