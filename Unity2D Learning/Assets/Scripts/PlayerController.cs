b93d3151f1126b7d27815b4b7402ba083beb09e4
chaz-ratima <chaz.ratima@gmail.com>
2022-08-03 03:02:20 +1200

    Add animations to movements
    Idle animation.
    Running either side has animation facing which horizontal way.
    Jump animation.

--- Unity2D Learning/Assets/Scripts/PlayerController.cs
+++ Unity2D Learning/Assets/Scripts/PlayerController.cs
@@ -13,14 +13,16 @@ public class PlayerController : MonoBehaviour
     private float moveHorizontal;
     private bool moveVertical;
 
+    public Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
         rb2D = gameObject.GetComponent<Rigidbody2D>();
 
         // Set variables
-        moveSpeed = 1f;
-        jumpForce = 5f;
+        moveSpeed = 2f;
+        jumpForce = 15f;
         isJumping = false;
         jumpCount = 2;
     }
@@ -28,6 +30,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         // Check for input
         moveHorizontal = Input.GetAxis("Horizontal");
         moveVertical = Input.GetKeyDown(KeyCode.Space);
@@ -35,14 +38,19 @@ public class PlayerController : MonoBehaviour
         // Vertical movement
         if (jumpCount != 0 && moveVertical == true)
         {
-                rb2D.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            rb2D.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
             // lowers jumpCount by 1 for each "space" jump
-                jumpCount--;
+            jumpCount--;
+            animator.SetBool("IsJumping", true);
         }
     }
 
     void FixedUpdate()
     {
+        animator.SetFloat("Speed", Mathf.Abs(moveHorizontal));
+        bool flipped = moveHorizontal < 0;
+        this.transform.rotation = Quaternion.Euler(new Vector3(0f, flipped ? 180f : 0f, 0f));
+
         // Horizontal movement
         if (moveHorizontal > 0.1f || moveHorizontal < 0.1f)
         {
@@ -58,6 +66,8 @@ public class PlayerController : MonoBehaviour
             isJumping = false;
             // Resets jumpCount
             jumpCount = 2;
+
+            animator.SetBool("IsJumping", false);
         }
     }
 
